2615e37f6bdfa9c34cedeba6634a22db82f1bb29
Arundhati <arundhatichodankar10@gmail.com>
2024-05-07 18:57:56 +0530

    data with network and response setup completed

---
+++ lib/data/app_exceptions.dart
@@ -0,0 +1,35 @@
+class AppExceptions implements Exception {
+  final String? _prefix, _message;
+
+  AppExceptions([this._message, this._prefix]);
+
+
+  @override
+  String toString() {
+    return "$_prefix: $_message";
+  }
+
+}
+
+class InternetException extends AppExceptions {
+  InternetException([String? message]) : super(message, "No Internet Connection");
+}
+
+class ServerTimeOut extends AppExceptions {
+  ServerTimeOut([String? message]) : super(message, "Server Timeout");
+}
+
+class BadRequestException extends AppExceptions {
+  BadRequestException([String? message]) : super(message, "Bad Request Exception");
+}
+
+
+class InvalidInputException extends AppExceptions {
+  InvalidInputException([String? message])
+      : super(message, "Invalid input Exception Exception");
+}
+
+class UnauthorizedException extends AppExceptions {
+  UnauthorizedException([String? message])
+      : super(message, "Unauthorized Exception");
+}
---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'dart:io';
+
+import '../app_exceptions.dart';
+import 'base_api_services.dart';
+import 'package:http/http.dart' as http;
+
+class NetworkApiServices extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic jsonResponse;
+    try {
+      final response =
+          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      jsonResponse = returnJsonResponse(response);
+    } on SocketException {
+      throw InternetException("NO Internet is available right now");
+    }
+    return jsonResponse;
+  }
+
+  @override
+  Future getPostApiResponse(String url, data) async {
+    dynamic jsonResponse;
+    try {
+      final response = await http
+          .post(Uri.parse(url), body: data)
+          .timeout(const Duration(seconds: 10));
+      jsonResponse = returnJsonResponse(response);
+    } on SocketException {
+      throw InternetException("NO Internet is available right now");
+    }
+    return jsonResponse;
+  }
+}
+
+dynamic returnJsonResponse(http.Response response) {
+  switch (response.statusCode) {
+    case 200:
+      dynamic jsonResponse = jsonDecode(response.body);
+      return jsonResponse;
+    case 400:
+      throw BadRequestException("Bad request");
+    default:
+      throw InternetException(
+          "${response.statusCode} : ${response.reasonPhrase}");
+  }
+}
